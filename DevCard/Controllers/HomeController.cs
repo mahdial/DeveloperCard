99f35a08352aa5815f7bb0001dcba098ab9e1296
mahdi alizadeh <Mahdi.al@gmail.com>
2021-09-21 14:35:33 +0430

    Add contact page and implement the client and server validation

--- DevCard/Controllers/HomeController.cs
+++ DevCard/Controllers/HomeController.cs
@@ -16,6 +16,29 @@ namespace DevCard.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            var model = new Contact();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Contact(Contact model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.error = "اطلاعات وارد شده صحیح نیست، لطفا دوباره تلاش کنید";
+                return View(model);
+            }
+            else
+            {
+                ViewBag.success = "اطلاعات وارد شده با موفقیت ثبت گردید";
+                return View();
+            }
+
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ DevCard/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "این فیلد اجباریست")]
+        [MinLength(3, ErrorMessage = "حداقل باید 3 کاراکتر باشد")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "ایمیل صحیح نمی باشد")]
+        [Required(ErrorMessage = "این فیلد اجباریست")]
+        public string Email { get; set; }
+
+        public string Service { get; set; }
+
+        [Required(ErrorMessage = "این فیلد اجباریست")]
+        public string Message { get; set; }
+    }
+}
